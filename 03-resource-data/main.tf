e7fa33bd4132bbf742672bf10032f0adb924e164
Padella <execve@kakao.com>
2021-12-26 02:35:18 +0900

    feat: ami 를 data source 를 통해 받아오도록 변경 + 최신 ami 를 받아오는 코드 작성

--- 03-resource-data/main.tf
+++ 03-resource-data/main.tf
@@ -2,8 +2,28 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+data "aws_ami" "ubuntu" {
+  # return 되는 여러 ami 이미지 중 최신 이미지를 사용한다.
+  most_recent = true
+
+  # ubuntu 20.04 의 모든 ami 이미지를 가져온다. 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  # 가상화 타입이 hvm 인 이미지
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # Canonical(ubuntu provider) 이 제작한 ami
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "ubuntu" {
-  ami           = "ami-04876f29fd3a5e8ba"
+  ami           = data.aws_ami.ubuntu.image_id
   instance_type = "t2.micro"
 
   tags = {

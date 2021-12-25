dd04742a8968664cdb66d95276f8c6dcd37697a5
Padella <execve@kakao.com>
2021-12-26 01:46:46 +0900

    feat: 01 file read + data source 를 사용해 로컬 파일 read

--- 01-start/main.tf
+++ 01-start/main.tf
@@ -6,3 +6,11 @@ resource "local_file" "foo" {
   filename = "${path.module}/foo.txt"
   content  = "Hello World!" 
 }
+
+data "local_file" "bar" {
+  filename = "${path.module}/bar.txt"
+}
+
+output "file_bar" {
+  value = data.local_file.bar
+}

ca5e5c1daee0ccd4625bfd5a4339af38c052cc74
Padella <execve@kakao.com>
2021-12-26 03:41:43 +0900

    feat: static 하게 iam user 를 추가하는 코드 작성

---
+++ 06-count-for-each/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+/*
+ * No count / for_each
+ */
+resource "aws_iam_user" "user_1" {
+  name = "user-1"
+}
+
+resource "aws_iam_user" "user_2" {
+  name = "user-2"
+}
+
+resource "aws_iam_user" "user_3" {
+  name = "user-3"
+}
+
+output "user_arns" {
+  value = [
+    aws_iam_user.user_1.arn,
+    aws_iam_user.user_2.arn,
+    aws_iam_user.user_3.arn
+  ]
+}

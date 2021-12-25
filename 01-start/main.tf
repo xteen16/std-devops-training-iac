050a32584173e0b9c059aacdccc78e45abc1a08c
Padella <execve@kakao.com>
2021-12-26 01:56:24 +0900

    feat: aws vpc resource + vpc 생성  resource 추가

--- 01-start/main.tf
+++ 01-start/main.tf
@@ -1,6 +1,10 @@
 provider "local" {
 }
 
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
 resource "local_file" "foo" {
   # path.module 은 .tf 파일이 위치한 경로
   filename = "${path.module}/foo.txt"
@@ -13,4 +17,19 @@ data "local_file" "bar" {
 
 output "file_bar" {
   value = data.local_file.bar
+}
+
+resource "aws_vpc" "foo" {
+  // cidr_block = "10.0.0.0/16"
+  # cidr_block 같은 option 을 변경할 경우 terraform 이 
+  # resource 를 삭제한 뒤 재생성한다.
+  cidr_block = "10.123.0.0/16"
+
+  tags = {
+    "Name" = "This is test vpc"
+  }
+}
+
+output "vpc_foo" {
+  value = aws_vpc.foo
 }

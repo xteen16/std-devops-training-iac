609a7d1990b3898d6fa7c7db34d9f15cd6a31331
Padella <execve@kakao.com>
2021-12-28 19:47:32 +0900

    feat: iam user 에 권한 부여 코드 작성 + for_each 문을 사용해 iam user 에게 권한 부여

--- 08-for/main.tf
+++ 08-for/main.tf
@@ -44,6 +44,7 @@ resource "aws_iam_user" "this" {
 }
 
 resource "aws_iam_user_group_membership" "this" {
+  # role 에 따라 group 분리
   for_each = {
     for user in var.users :
     user.name => user
@@ -53,3 +54,34 @@ resource "aws_iam_user_group_membership" "this" {
   groups = each.value.is_developer ? [aws_iam_group.developer.name, aws_iam_group.employee.name] : [aws_iam_group.employee.name]
 }
 
+locals {
+  developers = [
+    for user in var.users :
+    user
+    if user.is_developer
+  ]
+}
+
+resource "aws_iam_user_policy_attachment" "developer" {
+  # group 에 맞는 권한 부여
+  for_each = {
+    for user in local.developers :
+    user.name => user
+  }
+
+  user       = each.key
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+
+  # 의존성 관리
+  depends_on = [
+    aws_iam_user.this
+  ]
+}
+
+output "high_level_users" {
+  value = [
+    for user in var.users :
+    user
+    if user.level > 5
+  ]
+}

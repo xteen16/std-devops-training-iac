1e900d5fa9ad3dab776354b0ae18fa78dfb32f90
Padella <execve@kakao.com>
2021-12-28 19:41:57 +0900

    feat: iam user list 를 받아 사용자 및 그룹을 추가하는 코드 작성 + iam user list 파일 추가 + iam group 생성 코드 추가 + iam user role 에 맞는 그룹에 추가하는 코드 추가

---
+++ 08-for/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+/*
+ * Groups
+ */
+
+resource "aws_iam_group" "developer" {
+  name = "developer"
+}
+
+resource "aws_iam_group" "employee" {
+  name = "employee"
+}
+
+output "groups" {
+  value = [
+    aws_iam_group.developer,
+    aws_iam_group.employee,
+  ]
+}
+
+/*
+ * Users
+ */
+
+variable "users" {
+  type = list(any)
+}
+
+resource "aws_iam_user" "this" {
+  for_each = {
+    for user in var.users :
+    user.name => user
+  }
+
+  name = each.key
+
+  tags = {
+    level = each.value.level
+    role  = each.value.role
+  }
+}
+
+resource "aws_iam_user_group_membership" "this" {
+  for_each = {
+    for user in var.users :
+    user.name => user
+  }
+
+  user   = each.key
+  groups = each.value.is_developer ? [aws_iam_group.developer.name, aws_iam_group.employee.name] : [aws_iam_group.employee.name]
+}
+

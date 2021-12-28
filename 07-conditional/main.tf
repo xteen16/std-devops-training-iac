f426b44adfed85dea079e9069942d88fe6abe60d
Padella <execve@kakao.com>
2021-12-28 19:22:12 +0900

    feat: count 를 통한 resource 생성 + count 와 삼항연산자 트릭을 이용해 resource 생성 제어

--- 07-conditional/main.tf
+++ 07-conditional/main.tf
@@ -19,3 +19,23 @@ locals {
 output "message" {
   value = local.message
 }
+
+
+/*
+ * Count Trick for Conditional Resource
+ */
+variable "internet_gateway_enabled" {
+  type    = bool
+  default = true
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "this" {
+  # internet gateway 변수에 따라서 해당 resource 를 생성한다.
+  count = var.internet_gateway_enabled ? 1 : 0
+
+  vpc_id = aws_vpc.this.id
+}
